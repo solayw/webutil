5163fdda15c0b14b985de3c6818ff0632aac25fc
Adam Williams <sam384sp@gmail.com>
2021-07-11 19:55:41 +0800

    jpa json converter

---
+++ spring-web/src/main/java/com/github/solayw/webutil/Order.java
@@ -0,0 +1,13 @@
+package com.github.solayw.webutil;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Order
+{
+    int value();
+}
